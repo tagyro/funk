b4f96b412b841cf4b9e0ed23c06ebf7ade847a06
Andrei Stoleru <astoleru@ambidexter.gmbh>
2019-12-05 13:01:12 +0100

    updated function

--- src/lambda/appstore.js
+++ src/lambda/appstore.js
@@ -1,13 +1,11 @@
 import fetch from "node-fetch"
-import xml2js from 'xml2js-es6-promise'
 
 exports.handler = async (event, context, callback) => {
-    const mediumFeed = `https://itunes.apple.com/de/rss/customerreviews/id=1438105898/sortBy=mostRecent/xml`
-    const response = await fetch(mediumFeed)
-    const xml = await response.text()
-    const data = await xml2js(xml)
+    const behanceEndpoint = `https://itunes.apple.com/de/rss/customerreviews/id=1438105898/sortBy=mostRecent/xml`
+    const response = await fetch(behanceEndpoint)
+    const data = await response.json()
     return  {
       statusCode: 200,
-      body: JSON.stringify(data.rss.channel[0].item)
+      body: JSON.stringify(data.projects)
     }
 }
