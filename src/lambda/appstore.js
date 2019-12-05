50ef3be73ada2e49b387f2037bd7952c23892c62
Andrei Stoleru <astoleru@ambidexter.gmbh>
2019-12-05 15:46:11 +0100

    updated function

--- src/lambda/appstore.js
+++ src/lambda/appstore.js
@@ -1,11 +1,11 @@
 import fetch from "node-fetch"
 
 exports.handler = async (event, context, callback) => {
-    const behanceEndpoint = `https://itunes.apple.com/de/rss/customerreviews/id=1438105898/sortBy=mostRecent/json`
-    const response = await fetch(behanceEndpoint)
+    const endpoint = `https://itunes.apple.com/de/rss/customerreviews/id=1438105898/sortBy=mostRecent/json`
+    const response = await fetch(endpoint)
     const data = await response.json()
     return  {
       statusCode: 200,
-      body: JSON.stringify(data.projects)
+      body: JSON.stringify(data)
     }
 }
